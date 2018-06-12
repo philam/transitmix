5b9396e9ba594ba925373a243087b7679bfdd4c3
Phil <philam@users.noreply.github.com>
2018-06-12 12:31:17 +1000

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,8 @@ _.templateSettings = {
 
 $(document).ready(function() {
   var options = { tileLayer: { detectRetina: true } };
-  app.leaflet = L.mapbox.map('map', 'mapbox.streets', options);
+  app.leaflet = L.mapbox.accessToken = 'pk.eyJ1IjoicGhpbGlwZGFyIiwiYSI6ImNqZmx1N2wzbTBva2ozM21rbnFjN21uZDgifQ.tK-myvugjB-kOzfpAFB0DA';
+    L.mapbox.map('map', 'mapbox.streets', options);
   
   // Feedback View that we want visible at all times
   var feedbackView = new app.FeedbackView();
